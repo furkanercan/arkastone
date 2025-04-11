817cc73ac7955ae0521eceeaca922631f27f2a2d
Furkan Ercan <furkanercan88@gmail.com>
2025-04-10 20:11:16 -0400

    added tests for functional blocks for encoder chain

---
+++ tests/coding/test_rnti_scrambling.py
@@ -0,0 +1,8 @@
+from src.coding.polar.nr5g.components.rnti_scrambling import *
+
+def test_rnti_scrambling():
+    crc = [0] * 24
+    rnti = 0xABCD
+    scrambled = rnti_scramble_crc(crc, rnti)
+    assert scrambled[:8] == [0]*8
+    assert scrambled[8:] == [(rnti >> i) & 1 for i in reversed(range(16))]
